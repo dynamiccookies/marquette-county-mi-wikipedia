46540ece76bb73bd54212787784a95a91111e135
Chad Davis <dynamiccookies@users.noreply.github.com>
2021-06-08 13:46:19 -0500

    Added ability to account for bold values
    Added the ability to account for the bold values that show updated values for the current year.
    Removed ranking column since it was confusing when numbers were out of order.
    Closes #2

--- top-employers.js
+++ top-employers.js
@@ -7,19 +7,22 @@ var today = new Date().toLocaleDateString("en-us",{year:'numeric',month:'long',d
 results = "===Top employers===\n<!-- Please use the script referenced on the talk page to update these tables -->\n" + 
 	"''Last updated " + today + ".''\n\n" + 
 	"According to the Lake Superior Community Partnership website,<ref>{{cite web |url=https://marquette.org/principal-employers/ |title=" + 
-  article.getElementsByTagName("h1")[0].innerText + " |website=Lake Superior Community Partnership |access-date=" + today + 
+	article.getElementsByTagName("h1")[0].innerText + " |website=Lake Superior Community Partnership |access-date=" + today + 
 	"}}</ref> the top employers in the county are:\n\n";
 
 if(article.getElementsByTagName("h3")[0].innerText == "Private Employers:"){
     private = article.getElementsByTagName("table")[0].getElementsByTagName("tbody")[0].getElementsByTagName("tr");
-	results += "{|\n|- valign='top'\n|\n{| class='wikitable sortable'\n|-\n! #\n! Private Employer\n! # of Employees";
+	results += "{|\n|- valign='top'\n|\n{| class='wikitable sortable'\n|-\n! Private Employer\n! # of Employees";
 
 	for (row = 1; row < private.length; row++) {
 
 		employer  = private[row].getElementsByTagName("td")[0].innerText;
-		employees = private[row].getElementsByTagName("td")[1].innerText;
+		employees = private[row].getElementsByTagName("td")[1];
 
-		results += "\n|-\n| " + row + "\n|" + employer + "\n|" + employees;
+		if(employees.innerHTML.startsWith("<strong>")){employees = "'''" + employees.innerText + "'''";} 
+		else {employees = employees.innerText;}
+
+		results += "\n|-\n|" + employer + "\n|" + employees;
 	}
 
 	results += "\n|}\n| &nbsp;&nbsp;&nbsp;&nbsp;\n|\n";
@@ -27,17 +30,20 @@ if(article.getElementsByTagName("h3")[0].innerText == "Private Employers:"){
 
 if(article.getElementsByTagName("h3")[1].innerText == "Public Employers:"){
     public = article.getElementsByTagName("table")[1].getElementsByTagName("tbody")[0].getElementsByTagName("tr");
-	results += "{| class='wikitable sortable'\n|-\n! #\n! Public Employer\n! # of Employees";
+	results += "{| class='wikitable sortable'\n|-\n! Public Employer\n! # of Employees";
 
 	for (row = 1; row < public.length; row++) {
 
 		employer  = public[row].getElementsByTagName("td")[0].innerText;
-		employees = public[row].getElementsByTagName("td")[1].innerText;
+		employees = public[row].getElementsByTagName("td")[1];
 
-		results += "\n|-\n| " + row + "\n|" + employer + "\n|" + employees;
+		if(employees.innerHTML.startsWith("<strong>")){employees = "'''" + employees.innerText + "'''";} 
+		else {employees = employees.innerText;}
+
+		results += "\n|-\n|" + employer + "\n|" + employees;
 	}
 
-	results += "\n|}\n|}\n";
+	results += "\n|}\n|}\n<nowiki>*</nowiki>Bolded values have been updated for the current year.";
 }
 
 console.log(results);
